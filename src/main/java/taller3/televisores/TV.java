b563faefc0c845155abdf6b0a5f52769278f0b05
LuaN1409 <141962309+LuaN1409@users.noreply.github.com>
2023-08-28 19:42:39 -0500

    Update TV.java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -2,16 +2,19 @@ package taller3.televisores;
 
 public class TV {
 	private Marca marca;
-	private int canal = 1;
-	private int precio = 500;
+	private int canal;
+	private int precio;
 	private boolean estado;
-	private int volumen = 1;
+	private int volumen;
 	private Control control;
 	private static int numTV = 0;
 	
 	public TV(Marca marca, boolean estado) {
 		this.marca = marca;
 		this.estado = estado;
+		this.canal = 1;
+		this.volumen = 1;
+		this.precio = 500;
 		numTV++;
 	}
 	
@@ -63,8 +66,8 @@ public class TV {
 		return numTV;
 	}
 	
-	public static void setNumTV(int numTV) {
-		return;
+	public static void setNumTV(int value) {
+		numTV = value;
 	}
 	
 	public void turnOn() {
